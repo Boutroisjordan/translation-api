66db0674fff210ddfb3f43e20def12d7069f4c0a
Clément Legay <delta80@outlook.fr>
2024-07-25 09:36:46 +0200

    feat: trying a cors configuration

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 import flask, ollama
+from flask import make_response
+from flask_cors import cross_origin, CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+cors = CORS(app, resources={r"/api/*": {"origins": "*", "methods": ["POST", "OPTIONS"]}})
+
+class middleware():
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if environ['REQUEST_METHOD'] == 'OPTIONS':
+            start_response(
+                '200 OK',
+                [
+                    ('Content-Type', 'text/plain'),
+                    ('Content-Length', '0'),
+                    ('Access-Control-Allow-Origin', '*'),
+                    ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'),
+                    ('Access-Control-Allow-Headers', 'Content-Type'),
+                    ('Access-Control-Max-Age', '86400'),
+                ]
+            )
+        return self.app(environ, start_response)
+
+
+app.wsgi_app = middleware(app.wsgi_app)
+
+
+@app.route("/api/translate", methods=["POST"])
+@cross_origin()
+def translate():
+    content = flask.request.json['content']
+    outlang = flask.request.json['outLang']
+    result = ollama.chat(model='mistral', messages=[
+        {
+            'role': 'user',
+            'content': f'Translate in {outlang} this: "{content}". The return type should be in JSON format with'
+                       ' the key "content" and the value the translated text. You must define whether the text is'
+                       ' explicit or not and the language of the text. Example: {"content": the translation, '
+                       '"explicit": if the text seems explicit (true/false), "inLang": language you detected (ja), '
+                       '"outLang": given outlang (fr)}. Type your response as a JSON and don\'t write anything '
+                       'else.',
+        },
+    ])
+    print(result['message']['content'])
+    return result['message']['content']
+
 
 
 @app.route('/llama3', methods=['GET'])
@@ -12,24 +58,7 @@ def llama():
             'content': 'Translate in englsih this and response in json: 向いているものはありません',
         },
     ])
-    print(response['message']['content'])
-    return response['message']['content']
-
-
-@app.route('/translate', methods=['POST'])
-def translate():
-    content = flask.request.json['content']
-    outlang = flask.request.json['outLang']
-    response = ollama.chat(model='mistral', messages=[
-        {
-            'role': 'user',
-            'content': f'Translate in {outlang} this: "{content}". The return type should be in JSON format with'
-                       ' the key "content" and the value the translated text. You must define whether the text is'
-                       ' explicit or not and the language of the text. Example: {"content": the translation, '
-                       '"explicit": if the text seems explicit (true/false), "inLang": language you detected (ja), '
-                       '"outLang": given outlang (fr)}. Type your response as a JSON and don\'t write anything else.',
-        },
-    ])
+    response.headers.add("Access-Control-Allow-Origin", "*")
     print(response['message']['content'])
     return response['message']['content']
 
